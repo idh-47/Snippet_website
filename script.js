f971e4ecdf4092d6c02436d9d265cbbedf7440f3
Yahya <idlahcencih@gmail.com>
2025-01-01 23:10:21 +0100

    footer added and nav in js

--- script.js
+++ script.js
@@ -11,3 +11,34 @@
 
 // btn.addEventListener('click', show);
 // del.addEventListener('click',hide);
+
+// nav bar configuration
+
+document.getElementById('service').addEventListener('mouseover', () =>{
+    document.getElementById('nav_container').style.display = 'none';
+    document.getElementById('scroll-nav').style.display = 'flex';
+});
+document.getElementById('mouse-e').addEventListener('mouseover', () =>{
+    document.getElementById('nav_container').style.display = 'none';
+    document.getElementById('scroll-nav').style.display = 'flex';
+});
+document.getElementById('hero').addEventListener('mouseover', () => {
+    document.getElementById('nav_container').style.display = 'flex';
+    document.getElementById('scroll-nav').style.display = 'none';
+})
+
+
+                            
+                                //phone
+
+document.getElementById('service').addEventListener('ontouchmove', () =>{
+    document.getElementById('nav_container').style.display = 'none';
+    document.getElementById('scroll-nav').style.display = 'flex';
+});
+document.getElementById('hero').addEventListener('ontouchmove', () => {
+    document.getElementById('nav_container').style.display = 'flex';
+    document.getElementById('scroll-nav').style.display = 'none';
+})
+
+
+// burger menu
