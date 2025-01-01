fa6f474bb9d5334c1f31eaf3519130ea2aa4c7b8
Yahya <idlahcencih@gmail.com>
2025-01-01 19:29:03 +0100

    added city section

---
+++ script.js
@@ -0,0 +1,13 @@
+// let btn = document.getElementById('menu');
+// let del = document.getElementById('del');
+// let menu = document.querySelector('section');
+
+// function show(){
+//  menu.style.display = 'static';
+// }
+// function hide(){
+//     menu.style.display = 'none';
+// }
+
+// btn.addEventListener('click', show);
+// del.addEventListener('click',hide);
